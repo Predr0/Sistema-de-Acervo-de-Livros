a97dc3774aeebdf9434cad03833e34be91871704
PedroSpindola <pedrojf04@gmail.com>
2024-10-19 14:10:47 -0300

    criação da classe

---
+++ acervo.py
@@ -0,0 +1,9 @@
+class Livro:
+
+    def __init__(self,titulo,autor,editora,quantidade,preco):
+
+        self.titulo = titulo;
+        self.autor = autor;
+        self.editora = editora;
+        self.quantidade = quantidade
+        self.preco = preco
