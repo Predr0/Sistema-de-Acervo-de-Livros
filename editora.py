dd2879d5bae62c492d36be292cfff9cb8d9596ef
PedroSpindola <pedrojf04@gmail.com>
2024-11-09 23:28:34 -0300

    final crud editor e genero

--- editora.py
+++ editora.py
@@ -1,13 +1,62 @@
-class Editora:
+from conexao import Conexao
+class Editora(Conexao):
 
-    def __init__(self,nome):
-       self._nome = nome;
+    def __init__(self):
+     Conexao.__init__(self)
 
-    def getNome(self):
+    def addEditora(self,*args):
+       try:
+         sql = "insert into editora(nome)values(%s)";
+         self.execute(sql,args)
+         self.commit()
+       except Exception as e:
+          print("erro ao inserir", e)
+   
+    def pesquisar(self, *args, type_s = "nome"):
+      sql = "select * from editora WHERE nome LIKE %s"
+      if type_s == "id":
+         sql = "SELECT * FROM editora WHERE id = %s"
+      self.cursor.execute(sql, args)
+      data = self.cursor.fetchall()
+      if data :
+         return data
+      else:
+         return "Registro nao encontrado"
 
-        return self._nome;
-    def setNome(self, nome):
-        if isinstance(nome, str):
-            self._nome = nome;
-        else:
-            print("erro");
+    def update(self, id, *args):
+      try:
+
+         sql = f"update editora SET nome = %s WHERE id = {id}"
+         self.execute(sql, args)
+         self.commit()
+         print("registro atualizado")
+      
+      except Exception as e:
+         print("erro ao atualizar",e)
+          
+    def delete(self, s):
+      try:
+         sql_s = f"select * from editora where nome= '{s}' "
+         if not self.query(sql_s):
+            return "Registro não encontrado para deletar"
+         sql_d = f"delete from editora where nome= '{s}'"
+         self.execute(sql_d)
+         self.commit()
+         return "Registro realizado com sucesso"
+      except Exception as e:
+          print("erro ao deletar", e)
+    def delGenero(self, id):
+      try:
+         sql_s = f"select * from editora where id= {id}"
+         if not self.query(sql_s):
+            return "Registro não encontrado para deletar"
+         sql_d = f"delete from editora where id={id}"
+         self.execute(sql_d)
+         self.commit()
+         return "Registro realizado com sucesso"
+      except Exception as e:
+          print("erro ao deletar", e)
+if __name__ == "__main__":
+      
+   e2 = Editora();
+   e2.addEditora("panini")
--- genero.py
+++ genero.py
@@ -13,12 +13,35 @@ class Genero(Conexao):
          self.commit()
        except Exception as e:
           print("erro ao inserir", e)
+   
+   def pesquisar(self, *args, type_s = "nome"):
+      sql = "select * from genero WHERE nome LIKE %s"
+      if type_s == "id":
+         sql = "SELECT * FROM genero WHERE id = %s"
+      self.cursor.execute(sql, args)
+      data = self.cursor.fetchall()
+      if data :
+         return data
+      else:
+         return "Registro nao encontrado"
+
+   def update(self, id, *args):
+      try:
+
+         sql = f"update genero SET nome = %s WHERE id = {id}"
+         self.execute(sql, args)
+         self.commit()
+         print("registro atualizado")
+      
+      except Exception as e:
+         print("erro ao atualizar",e)
+          
    def delete(self, s):
       try:
-         sql_s = f"select * from genero where nome= {s} "
+         sql_s = f"select * from genero where nome= '{s}' "
          if not self.query(sql_s):
             return "Registro não encontrado para deletar"
-         sql_d = f"delete from genero where nome= {s}"
+         sql_d = f"delete from genero where nome= '{s}'"
          self.execute(sql_d)
          self.commit()
          return "Registro realizado com sucesso"
@@ -26,7 +49,7 @@ class Genero(Conexao):
           print("erro ao deletar", e)
    def delGenero(self, id):
       try:
-         sql_s = f"select * from genero where id= '{id}'"
+         sql_s = f"select * from genero where id= {id}"
          if not self.query(sql_s):
             return "Registro não encontrado para deletar"
          sql_d = f"delete from genero where id={id}"
@@ -38,4 +61,4 @@ class Genero(Conexao):
 if __name__ == "__main__":
       
    g2 = Genero();
-   g2.delete("comedia")
+   print(g2.pesquisar(1, type_s="id"))
